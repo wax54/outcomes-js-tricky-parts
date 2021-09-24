7e114298be23ec986d980402d66134fa59a3c326
wax54 <wax654@gmail.com>
2021-09-23 19:09:19 -0700

    guessing game done

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,20 @@
 function guessingGame() {
+    const secretNum = Math.floor(Math.random() * 100);    
+    let guessCount = 0;
+    let gameOver = false;
 
+    return function game(guess) {
+        if(gameOver) return "The game is over, you already won!";
+        guessCount++;
+        if(guess > secretNum) {
+            return `${guess} is too high!`;
+        } else if(guess < secretNum) {
+            return `${guess} is too low!`;
+        } else {
+            gameOver = true;
+            return `You win! You found ${secretNum} in ${guessCount} guesses.`
+        }
+    }
 }
 
 module.exports = { guessingGame };
