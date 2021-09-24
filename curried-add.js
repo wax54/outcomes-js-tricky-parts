d981e519c943599765ac8bf8c039ae3a07a28596
wax54 <wax654@gmail.com>
2021-09-23 19:35:39 -0700

    curried adder complete

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,17 @@
-function curriedAdd(total) {
+function curriedAdd(total = 0) {
+    function adder(num) {
+        if(arguments.length){
+            total += num;
+            return adder
+        } else {
+            return total;
+        }
+    }
 
+    if(arguments.length)
+        return adder
+    else 
+        return total;
 }
 
 module.exports = { curriedAdd };
